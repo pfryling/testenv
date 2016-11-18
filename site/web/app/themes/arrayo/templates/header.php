bf07d5e473f4435eac969afe5c87fbac109d3916
gretzky <fedsfilms@gmail.com>
2016-11-17 20:20:24 -0500

    update

--- site/web/app/themes/arrayo/templates/header.php
+++ site/web/app/themes/arrayo/templates/header.php
@@ -179,76 +179,56 @@
 <div class="mnav-container mobile-only">
   <div class="mnav">
     <ul>
-      <li><a href="#">Home</a></li>
-      <li><a href="http://marioloncarek.com">About</a>
+      <li><a href="#">Services</a>
         <ul>
-          <li><a href="#">School</a>
+          <li><a href="/">AgileData</a>
             <ul>
-              <li><a href="#">Lidership</a></li>
-              <li><a href="#">History</a></li>
-              <li><a href="#">Locations</a></li>
-              <li><a href="#">Careers</a></li>
+              <li><a href="#">Overview</a></li>
+              <li><a href="#">for FinTech</a></li>
+              <li><a href="#">for HealthTech</a></li>
+              <li><a href="#">for HighTech</a></li>
             </ul>
           </li>
           <li><a href="#">Study</a>
             <ul>
-              <li><a href="#">Undergraduate</a></li>
-              <li><a href="#">Masters</a></li>
-              <li><a href="#">International</a></li>
-              <li><a href="#">Online</a></li>
+              <li><a href="#">Overview</a></li>
+              <li><a href="#">for FinTech</a></li>
+              <li><a href="#">for HealthTech</a></li>
+              <li><a href="#">for HighTech</a></li>
             </ul>
           </li>
           <li><a href="#">Research</a>
             <ul>
-              <li><a href="#">Undergraduate research</a></li>
-              <li><a href="#">Masters research</a></li>
-              <li><a href="#">Funding</a></li>
+              <li><a href="#">Overview</a></li>
+              <li><a href="#">for FinTech</a></li>
+              <li><a href="#">for HealthTech</a></li>
+              <li><a href="#">for HighTech</a></li>
             </ul>
           </li>
           <li><a href="#">Something</a>
             <ul>
-              <li><a href="#">Sub something</a></li>
-              <li><a href="#">Sub something</a></li>
-              <li><a href="#">Sub something</a></li>
-              <li><a href="#">Sub something</a></li>
+              <li><a href="#">Overview</a></li>
+              <li><a href="#">for FinTech</a></li>
+              <li><a href="#">for HealthTech</a></li>
+              <li><a href="#">for HighTech</a></li>
             </ul>
           </li>
         </ul>
       </li>
-      <li><a href="#">News</a>
+      <li><a href="#">Industries</a>
         <ul>
-          <li><a href="#">Today</a></li>
-          <li><a href="#">Calendar</a></li>
-          <li><a href="#">Sport</a></li>
+          <li><a href="#">FinTech</a></li>
+          <li><a href="#">HealthTech</a></li>
+          <li><a href="#">HighTech</a></li>
         </ul>
       </li>
-      <li><a href="http://marioloncarek.com">Contact</a>
+      <li><a href="/company">Company</a></li>
+      <li><a href="/careers">Careers</a></li>
+      <li><a href="#">Contact</a>
         <ul>
-          <li><a href="#">School</a>
-            <ul>
-              <li><a href="#">Lidership</a></li>
-              <li><a href="#">History</a></li>
-              <li><a href="#">Locations</a></li>
-              <li><a href="#">Careers</a></li>
-            </ul>
+          <li>
+            <!-- form -->
           </li>
-          <li><a href="#">Study</a>
-            <ul>
-              <li><a href="#">Undergraduate</a></li>
-              <li><a href="#">Masters</a></li>
-              <li><a href="#">International</a></li>
-              <li><a href="#">Online</a></li>
-            </ul>
-          </li>
-          <li><a href="#">Study</a>
-            <ul>
-              <li><a href="#">Undergraduate</a></li>
-              <li><a href="#">Masters</a></li>
-              <li><a href="#">International</a></li>
-              <li><a href="#">Online</a></li>
-            </ul>
-          </li>
-          <li><a href="#">Empty sub</a></li>
         </ul>
       </li>
     </ul>
