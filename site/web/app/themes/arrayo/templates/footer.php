1fd2ad322f6acd72a4faa6ba210daa86d774ce27
gretzky <fedsfilms@gmail.com>
2016-11-18 10:23:38 -0500

    update

--- site/web/app/themes/arrayo/templates/footer.php
+++ site/web/app/themes/arrayo/templates/footer.php
@@ -508,7 +508,7 @@ function animateThis() {
   progress.animate({
     countNum: percentage
   }, {
-    duration: 500,
+    duration: 800,
     easing: 'swing',
     step: function() {
       // What todo on every count
