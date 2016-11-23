cb6b3d5ce031d0ad9a6a0a48e1af336143c4931d
gretzky <fedsfilms@gmail.com>
2016-11-23 11:16:14 -0500

    update

--- site/web/app/themes/arrayo/templates/footer.php
+++ site/web/app/themes/arrayo/templates/footer.php
@@ -43,21 +43,11 @@
                 <li class="ul-item">
                     <h3>Apply now-<br>for Now or Later</h3>
                     <p style="16px">Join Arrayo, Get on board</p>
+                    <button id="app-toggle" class="app-toggle">Apply Now</button>
+                <div id="app-reveal">
+                  <?php echo do_shortcode("[ninja_form id=7]"); ?>
+                </div>
 
-<button class="toggle-overlay">Apply Now</button>
-
-<aside>
-  <div class="outer-close toggle-overlay">
-    <a class="close"><span></span></a>
-    </div>
-      <div class="apply-form-wrap">
-      <div class="apply-form">
-        <h2>Apply Now-<br>for Now or Later</h2>
-        <h5>Join Arrayo, Get on Board</h5>
-        <div class='cb-input'>
-            <?php echo do_shortcode("[ninja_form id=4]"); ?>
-      </div>
-</aside>
                 </li>
               </ul>
             </div>
