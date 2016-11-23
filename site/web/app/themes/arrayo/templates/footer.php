179eef1a88d1f798a1c2ee17fb2d97aceaed1899
gretzky <fedsfilms@gmail.com>
2016-11-23 11:50:08 -0500

    update

--- site/web/app/themes/arrayo/templates/footer.php
+++ site/web/app/themes/arrayo/templates/footer.php
@@ -52,7 +52,7 @@
                           <div class="apply-form">
                             <h2>Apply Now-<br>for Now or Later</h2>
                             <h5>Join Arrayo, Get on Board</h5>
-                            div class='cb-input'>
+                            <div class='cb-input'>
                                 <?php echo do_shortcode("[ninja_form id=4]"); ?>
                           </div>
                     </aside>
