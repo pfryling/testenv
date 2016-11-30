45c3a38ae3e9962e335aedf542cae27a783e8d26
gretzky <fedsfilms@gmail.com>
2016-11-30 09:13:22 -0500

    update

--- site/web/app/themes/arrayo/templates/head.php
+++ site/web/app/themes/arrayo/templates/head.php
@@ -2,7 +2,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title></title>
+    <title><?php wp_title(''); ?></title>
     <meta name="keywords" content="">
     <meta name="twitter:card" content="" />
     <meta name="twitter:site" content="" />
