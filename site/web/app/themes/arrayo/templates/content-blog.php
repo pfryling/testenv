0ba16725a96ba6c528a0dc8eac2e775d062f41bc
gretzky <fedsfilms@gmail.com>
2016-12-05 09:41:37 -0500

    updatE

--- site/web/app/themes/arrayo/templates/content-blog.php
+++ site/web/app/themes/arrayo/templates/content-blog.php
@@ -4,40 +4,24 @@
         <section class="page-header">
             <p class="drpdwn">
               <?php the_field('pr_cs'); ?> |
-              <?php the_field('pr_date--abbr'); ?>
+              <?php the_field('date'); ?>
             </p>
             <h1><?php the_field('post_title'); ?></h1>
         </section>
-          <?php if( have_rows('cs') ): ?>
-            <section class="blog_section">
-            <?php while( have_rows('cs') ): the_row(); ?>
-              <h2><?php the_sub_field('cs_context_header'); ?></h2>
-              <p><?php the_sub_field('cs_context'); ?></p>
-              <hr>
-              <h2><?php the_sub_field('cs_phases_header'); ?></h2>
-              <p><?php the_sub_field('cs_phases'); ?></p>
-              <hr>
-              <h2><?php the_sub_field('cs_challenges_header'); ?></h2>
-              <p><?php the_sub_field('cs_challenges'); ?></p>
-              <hr>
-              <h2><?php the_sub_field('cs_outcome_header'); ?></h2>
-              <p><?php the_sub_field('cs_outcome'); ?></p>
-            <?php endwhile; ?>
-          </section>
-          <?php endif; ?>
-          <?php if( have_rows('pr') ): ?>
-          <section class="blog_section">
-            <?php while( have_rows('pr') ): the_row(); ?>
-              <h2><?php the_sub_field('pr_date'); ?></h2>
-              <p><?php the_sub_field('pr_content'); ?></p>
-              <hr>
-              <h2><?php the_sub_field('pr_about_div--header'); ?></h2>
-              <p><?php the_sub_field('pr_about_div'); ?></p>
-              <hr>
-              <h2><?php the_sub_field('pr_about--header'); ?></h2>
-              <p><?php the_sub_field('pr_about'); ?></p>
-            <?php endwhile; ?>
-          </section>
-          <?php endif; ?>
+        <section class="blog_section">
+          <?php the_field('content'); ?>
+        </section>
     </div>
 </div>
+<section class="contact-footer wow fadeInUp" id="contact">
+    <div class="wrapper">
+        <div class="text">
+            <h2>Ask Team Arrayo</h2>
+            <p>The quest for efficiency has essentially shifted from the manual delivery of services to the design of highly automated and integrated digital systems. What’s your digital ambition? What’s your primary digital focus? What user experience do you envision?</p>
+            <h5>Let's talk!</h5>
+        </div>
+        <div class="form">
+            <?php echo do_shortcode("[ninja_form id=1]"); ?>
+        </div>
+    </div>
+</section>
