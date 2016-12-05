ca7f480f2bd50c89e9599bdbf82610effe6b6146
gretzky <fedsfilms@gmail.com>
2016-12-05 16:08:54 -0500

    update

--- site/web/app/themes/arrayo/templates/content-resources.php
+++ site/web/app/themes/arrayo/templates/content-resources.php
@@ -6,11 +6,11 @@
                 <p style="margin-bottom:68px"><?php the_field('page_subheader'); ?></p>
             </section>
             <div id="blogfilters">
-              <button value="*">All</button>
-              <button value=".c_fintech">FinTech</button>
-              <button value=".c_healthtech">HealthTech</button>
-              <button value=".c_hightech">HighTech</button>
-              <button value=".c_press">Press Releases</button>
+              <button data-filter="*">All</button>
+              <button data-filter=".c_fintech">FinTech</button>
+              <button data-filter=".c_healthtech">HealthTech</button>
+              <button data-filter=".c_hightech">HighTech</button>
+              <button data-filter=".c_press">Press Releases</button>
             </div>
 
         <div id="blogcontainer">
