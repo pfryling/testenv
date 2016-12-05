f3352afab1afefc3962a2595f67658f1db851819
gretzky <fedsfilms@gmail.com>
2016-12-05 15:18:28 -0500

    update

--- site/web/app/themes/arrayo/templates/content-resources.php
+++ site/web/app/themes/arrayo/templates/content-resources.php
@@ -2,26 +2,26 @@
     <div class="blog">
         <div class="wrapper">
             <section class="page-header">
-                <h1>Resources</h1>
-                <p style="margin-bottom:68px">Subheader text</p>
+                <h1><?php the_field('page_header'); ?></h1>
+                <p style="margin-bottom:68px"><?php the_field('page_subheader'); ?></p>
             </section>
             <div id="blogfilters">
               <button data-filter="*">All</button>
-              <button data-filter=".fintech">FinTech</button>
-              <button data-filter=".healthtech">HealthTech</button>
-              <button data-filter=".hightech">HighTech</button>
-              <button data-filter=".press">Press Releases</button>
+              <button data-filter=".c_fintech">FinTech</button>
+              <button data-filter=".c_healthtech">HealthTech</button>
+              <button data-filter=".c_hightech">HighTech</button>
+              <button data-filter=".c_press">Press Releases</button>
             </div>
 
         <div id="blogcontainer">
             <?php $loop = new WP_Query( array( 'post_type' => 'resource', 'posts_per_page' => -1 ) ); ?>
             <?php while ( $loop->have_posts() ) : $loop->the_post(); ?>
-            <div class="blogitem <?php the_field('industry'); ?>">
+            <div class="blogitem <?php the_field('blog_class'); ?>">
                 <div class="blog-drpdwn">
                     <?php the_field('pr_cs'); ?>
                 </div>
                 <h3 class="blogtitle"><?php the_field('post_title'); ?></h3>
-                <p class="blogdesc"><?php the_field('blog_desc'); ?></p>
+                <p class="blogdesc"><?php the_field('post_desc'); ?></p>
                 <div class="link">
                     <a href="<?php the_permalink(); ?>">Learn more <i class="fa fa-arrow-circle-o-right"></i></a>
                 </div>
