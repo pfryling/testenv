90578b36f795c3bb632480c04830ff0f58d7b927
gretzky <fedsfilms@gmail.com>
2016-12-06 15:08:58 -0500

    update

--- site/web/app/themes/arrayo/templates/content-company.php
+++ site/web/app/themes/arrayo/templates/content-company.php
@@ -89,7 +89,38 @@
                    </div>
                     <div class="item">
                        <div class="cta-card">
-                        <img alt="AgileData Icon" src="http://imgh.us/agiledata-icon_1.svg" class="cta-icon">
+                        <svg style="width:50px;height:auto;" class="icon" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 77.41 82.88">
+                          <defs>
+                            <style>
+                              .cls-1 {
+                                fill: #4fd6ff;
+                              }
+                            </style>
+                          </defs>
+                          <title>agiledata-icon</title>
+                          <g id="graph-shape" class="pullRight">
+                            <path class="cls-1" d="M62.35,38.44H41.71V17.79H38.63a23.27,23.27,0,0,0-9.61,2,23.74,23.74,0,0,0-13.8,18.62H4v6H21v-3A18,18,0,0,1,31.48,25.28,17,17,0,0,1,35.71,24V44.44H56.12a17.19,17.19,0,0,1-1.89,5.5l5.26,2.89a23.54,23.54,0,0,0,2.87-11.4Z"/>
+                          </g>
+                          <g id="wedge" class="pullUp">
+                            <path class="cls-2" d="M62.65,34.39H45.75V17.49h2.51A14.32,14.32,0,0,1,62.65,31.89Zm-11.9-5h6.55a9.18,9.18,0,0,0-6.55-6.55Z"/>
+                          </g>
+                          <g id="dotted-line" class="rotate-alt">
+                            <g>
+                              <path class="cls-3" d="M24.61,59.55a24.27,24.27,0,0,1-6.71-9.12l5.53-2.32a18.24,18.24,0,0,0,5,6.85Z"/>
+                              <path class="cls-4" d="M39.7,65.11c-.65,0-1.31,0-2-.08a23.09,23.09,0,0,1-8.6-2.46l2.73-5.34a17.12,17.12,0,0,0,6.38,1.83,17.6,17.6,0,0,0,2.37,0l.32,6C40.53,65.1,40.12,65.11,39.7,65.11Z"/>
+                              <path class="cls-5" d="M46.34,64.16,44.65,58.4A18.12,18.12,0,0,0,52,54.16l4.14,4.34A24.15,24.15,0,0,1,46.34,64.16Z"/>
+                            </g>
+                          </g>
+                          <g id="circles" class="grow">
+                            <path class="cls-6" d="M38.71,11.48a5.75,5.75,0,1,1,2.42-.54A5.73,5.73,0,0,1,38.71,11.48ZM38.7,4a1.74,1.74,0,1,0,.59.1h0A1.73,1.73,0,0,0,38.7,4Zm0,78.88a5.74,5.74,0,1,1,2-11.13h0a5.74,5.74,0,0,1-2,11.13Zm0-7.47a1.73,1.73,0,1,0,.59.1h0A1.73,1.73,0,0,0,38.71,75.41Z"/>
+                          </g>
+                          <g id="outer_shapes" class="rotate" data-name="outer shapes">
+                            <g>
+                              <path class="cls-7" d="M19.16,74.84A38.73,38.73,0,0,1,.74,34,38.44,38.44,0,0,1,17.14,9.32l3.34,5a32.71,32.71,0,0,0,1.71,55.37Z"/>
+                              <path class="cls-8" d="M60.27,73.56l-3.34-5a32.71,32.71,0,0,0-1.71-55.37l3-5.18A38.73,38.73,0,0,1,76.67,48.9,38.44,38.44,0,0,1,60.27,73.56Z"/>
+                            </g>
+                          </g>
+                        </svg>
                         <p><?php the_field('slider--agiledata'); ?></p>
                         <div class="link">
                           <a href="/agiledata">Learn more <i class="fa fa-arrow-circle-o-right"></i></a>
