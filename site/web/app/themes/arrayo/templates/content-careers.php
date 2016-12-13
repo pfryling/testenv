455abdd6426ebd872b4ebdf92788817003f9aa71
gretzky <fedsfilms@gmail.com>
2016-12-13 15:04:06 -0500

    push

--- site/web/app/themes/arrayo/templates/content-careers.php
+++ site/web/app/themes/arrayo/templates/content-careers.php
@@ -170,42 +170,22 @@
         <div class="wrapper">
             <h3>Current Openings</h3>
             <h6>Filter</h6>
-            <div class="job-filter-wrap">
-
-              <div class="job-filters">
-                <h6>Industry</h6>
-                <div class="job-button-group js-radio-button-group" data-filter-group="color">
-                  <button class="job-button is-checked" data-filter="">any</button>
-                  <button class="job-button" data-filter=".fintech">fintech</button>
-                  <button class="job-button" data-filter=".healthtech">healthtech</button>
-                  <button class="job-button" data-filter=".hightech">hightech</button>
-                </div>
-              </div>
-
-              <div class="filters">
-                <h6>Domain</h6>
-                <div class="job-button-group js-radio-button-group" data-filter-group="size">
-                  <button class="job-button is-checked" data-filter="">any</button>
-                  <button class="job-button" data-filter=".agiledata">agiledata</button>
-                  <button class="job-button" data-filter=".rtech">rtech</button>
-                  <button class="job-button" data-filter=".databridge">databridge</button>
-                  <button class="job-button" data-filter=".connect">connect</button>
-                </div>
-              </div>
-
+            <div id="blogfilters">
+              <button data-filter="*">All</button>
+              <button data-filter=".c_fintech">FinTech</button>
+              <button data-filter=".c_healthtech">HealthTech</button>
+              <button data-filter=".c_hightech">HighTech</button>
+              <button data-filter=".c_press">Press Releases</button>
             </div>
-            <div class="job-results wow fadeInUp" id="Container">
+            <div id="blogcontainer">
                 <?php $loop = new WP_Query( array( 'post_type' => 'job-post', 'posts_per_page' => -1 ) ); ?>
                 <?php while ( $loop->have_posts() ) : $loop->the_post(); ?>
-                <div class="job-box <?php the_field('job_class'); ?>">
-                    <h4><?php the_field('job_title--header'); ?></h4>
+                <div class="blogitem <?php the_field('job_class'); ?>" data-category="<?php the_field('jobclass'); ?>">
                     <ul>
                         <li><strong>Industry:</strong> <?php the_field('job_industry'); ?></li>
                         <li><strong>Type:</strong> <?php the_field('job_type'); ?></li>
                         <li><strong>Seniority:</strong> <?php the_field('job_seniority'); ?></li>
                     </ul>
-                    <hr style="margin: 0 auto;width:95%;display:block;outline:0;height:1px;border:0;border-top:1px solid rgba(0,0,0,0.1)">
-                    <a href="<?php the_permalink(); ?>">Learn more <i class="fa fa-arrow-circle-o-right"></i></a>
                 </div>
                 <?php endwhile; wp_reset_query(); ?>
             </div>
