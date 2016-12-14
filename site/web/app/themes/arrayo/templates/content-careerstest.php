82edafe52524baa09b0f7a8591830f8865f25ec9
gretzky <fedsfilms@gmail.com>
2016-12-14 11:28:30 -0500

    test

--- site/web/app/themes/arrayo/templates/content-careerstest.php
+++ site/web/app/themes/arrayo/templates/content-careerstest.php
@@ -34,14 +34,15 @@
                 <?php $loop = new WP_Query( array( 'post_type' => 'job-post', 'posts_per_page' => -1 ) ); ?>
                 <?php while ( $loop->have_posts() ) : $loop->the_post(); ?>
                 <div class="jobitem <?php the_field('job_class'); ?>">
-                    <h4><?php the_field('job_title--header'); ?></h4>
+                    <h3><?php the_field('job_title--header'); ?></h3>
                     <ul>
                         <li><strong>Industry:</strong> <?php the_field('job_industry'); ?></li>
                         <li><strong>Type:</strong> <?php the_field('job_type'); ?></li>
                         <li><strong>Seniority:</strong> <?php the_field('job_seniority'); ?></li>
                     </ul>
-                    <hr style="margin: 0 auto;width:95%;display:block;outline:0;height:1px;border:0;border-top:1px solid rgba(0,0,0,0.1)">
-                    <a href="<?php the_permalink(); ?>">Learn more <i class="fa fa-arrow-circle-o-right"></i></a>
+                    <div class="link">
+                        <a href="<?php the_permalink(); ?>">Learn more <i class="fa fa-arrow-circle-o-right"></i></a>
+                    </div>
                 </div>
                 <?php endwhile; wp_reset_query(); ?>
             </div>
