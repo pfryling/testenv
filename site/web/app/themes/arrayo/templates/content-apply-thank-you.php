c8ec20668861fda294238042f9d0de4a1a6aa2f5
gretzky <fedsfilms@gmail.com>
2016-12-15 15:15:39 -0500

    update

--- site/web/app/themes/arrayo/templates/content-apply-thank-you.php
+++ site/web/app/themes/arrayo/templates/content-apply-thank-you.php
@@ -5,7 +5,7 @@
                 <h1>Apply Now</h1>
                 <p>for Now or Later</p>
             </section>
-        <?php echo do_shortcode("[ninja_form id=10]"); ?>
+        <?php echo do_shortcode("[ninja_form id=11]"); ?>
         </div>
         <section class="eeo">
             <div class="wrapper">
