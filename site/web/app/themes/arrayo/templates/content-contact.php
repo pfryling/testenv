85d389a358f5fb37a17aa611d87bee952057ce66
gretzky <fedsfilms@gmail.com>
2016-12-15 09:00:53 -0500

    update

---
+++ site/web/app/themes/arrayo/templates/content-contact.php
@@ -0,0 +1,9 @@
+<?php the_content(); ?>
+    <div class="apply-page-bg">
+        <div class="wrapper">
+            <section class="page-header">
+                <h1>Contact Us</h1>
+            </section>
+        </div>
+        <?php echo do_shortcode("[ninja_form id=1]"); ?>
+    </div>
