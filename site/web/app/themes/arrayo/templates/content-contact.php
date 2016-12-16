4252bfe0c9961f5ba7594cda5381b7c58daa3de0
gretzky <fedsfilms@gmail.com>
2016-12-16 11:27:03 -0500

    update

--- site/web/app/themes/arrayo/templates/content-contact.php
+++ site/web/app/themes/arrayo/templates/content-contact.php
@@ -5,7 +5,7 @@
                 <h1>Get in Touch</h1>
             </section>
             <div class='contact-page-form'>
-        <?php echo do_shortcode("[ninja_form id=1]"); ?>
+        <?php echo do_shortcode("[ninja_form id=13]"); ?>
         </div>
         </div>
     </div>
