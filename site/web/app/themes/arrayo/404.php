97fdc19b7319ca2b41dff9f59bb9d099e7cfa6de
gretzky <fedsfilms@gmail.com>
2016-12-19 13:23:51 -0500

    updatE

--- site/web/app/themes/arrayo/404.php
+++ site/web/app/themes/arrayo/404.php
@@ -6,7 +6,7 @@
             <h1>404</h1>
         </section>
         <section class="afp">
-            <p><?php _e('Sorry, but the page you were trying to view does not exist.', 'sage'); ?></p>
+            <p style="color:#ffffff"><?php _e('Sorry, but the page you were trying to view does not exist.', 'sage'); ?></p>
         </section>
         <div style="margin-bottom: 68px">&nbsp;</div>
     </div>
