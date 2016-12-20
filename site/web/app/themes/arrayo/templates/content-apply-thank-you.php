c28b2b664e4a487c13bc5e45a71fb2aa1a6fc93b
gretzky <fedsfilms@gmail.com>
2016-12-20 10:41:31 -0500

    update

--- site/web/app/themes/arrayo/templates/content-apply-thank-you.php
+++ site/web/app/themes/arrayo/templates/content-apply-thank-you.php
@@ -7,7 +7,7 @@
             </section>
                 <h3 class="apply-page-h">Thank you for your application.</h3>
                 <div class="moreinfo-wrap">
-                    <a class="moreinfobtn" style="color:#ffffff;" href="/apply-more-info">Please provide additional information (optional).<i style="color:#ffffff" class="fa fa-arrow-circle-right"></i></a>
+                    <a class="moreinfobtn" style="color:#ffffff;" href="/apply-more-info">Please provide additional information (optional).<i style="margin-right:30px;color:#ffffff" class="fa fa-arrow-circle-right"></i></a>
                 </div>
                 <div style="margin-bottom: 68px">&nbsp;</div>
             </section>
