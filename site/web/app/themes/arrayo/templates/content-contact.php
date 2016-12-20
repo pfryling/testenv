39109465fb3bd3cfddede06d6549cfecd5a71ccd
gretzky <fedsfilms@gmail.com>
2016-12-20 14:33:04 -0500

    updatE

--- site/web/app/themes/arrayo/templates/content-contact.php
+++ site/web/app/themes/arrayo/templates/content-contact.php
@@ -1,4 +1,5 @@
 <?php the_content(); ?>
+<div class="main">
     <div class="apply-page-bg">
         <div class="wrapper">
             <section class="page-header">
@@ -9,3 +10,4 @@
             </section>
         </div>
     </div>
+</div>
